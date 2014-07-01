c8b4c6bfea699d70d611895f5f0813c278fd1abc
Davide <bernardini.davide@yahoo.it>
2014-07-02 01:19:39 +0200

    sistemata la visualizzazione del gene, separando le stringhe del gene_assignment, rendendo clikkabili le voci di interesse (ancora da completare ovviamente)

--- gene-expression/controller/showGene.php
+++ gene-expression/controller/showGene.php
@@ -9,7 +9,7 @@
 		if (isset($_GET['gene_symbol']) && $_GET['gene_symbol']!="") {
 			$_SESSION['page_corrent'] = "showGene.php";
 			$tlp = new FastTemplate("../view");
-			$tlp->define(array('gene' => "showViewGene.html"));
+			$tlp->define(array('gene' => "showViewGene.html", 'elementGene'=>"elementGene.html"));
 		    $tlp->assign('NAME_GENE',$_GET['gene_symbol']);
 			if ($_SESSION['type']=='superuser') {
 				$tlp->assign('HOME',"superUserChoiceController.php");
@@ -18,8 +18,22 @@
 			$gene = $lg->retrieveGeneByGeneSymbol($_GET['gene_symbol']); 
 			if (isset($gene) && $gene!=NULL) {
 				$tlp->assign('GENE_SYMBOL',$gene->geneSymbol);
-				$tlp->assign('GENE_ASSIGNMENT',$gene->geneAssignment);
+				
 				$tlp->assign('REF_SEQ',$gene->refSeq);
+				$supex = explode("///", $gene->geneAssignment);
+				foreach($supex as $sup) {
+					$ex = explode("//", $sup);
+					foreach($ex as $el) {
+						if (strpos($el, 'NM_') !== false) {
+							$tlp->assign('GENE_ASSIGNMENT_ELEMENT',"<a href=''>".$el."</a>");
+						} else if(strpos($el, 'ENST') !== false) {
+							$tlp->assign('GENE_ASSIGNMENT_ELEMENT',"<a href=''>".$el."</a>");
+						} else {
+							$tlp->assign('GENE_ASSIGNMENT_ELEMENT',$el);
+						}
+							$tlp->parse('GENE_ASSIGNMENT',".elementGene");
+					}	
+				}
 			} else {
 				$tlp->assign('GENE_SYMBOL',"?");
 				$tlp->assign('GENE_ASSIGNMENT',"?");
