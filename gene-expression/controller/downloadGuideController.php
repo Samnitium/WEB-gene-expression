b676b8bf1624fdd38468ad3bab6dd3fe9e42dfe9
Davide <bernardini.davide@yahoo.it>
2014-07-11 12:10:53 +0200

    aggiunti i link dei crediti nella pagina di login e quello del download della guida nella pagina del profilo utente

---
+++ gene-expression/controller/downloadGuideController.php
@@ -0,0 +1,24 @@
+<?php
+	
+		
+	session_start();
+	if(isset($_SESSION['iduser'])) {
+		$fileLocation = "../../documentation/documentation.toc";
+		
+		header("Cache-Control: public");
+	  	header("Content-Description: File Transfer");
+	  	header("Content-Disposition: attachment; filename=".$fileLocation);
+	  	header("Content-Transfer-Encoding: binary");
+		//header("Content-type: application/pdf");
+	  // Leggo il contenuto del file
+	  	readfile($fileLocation);	
+		
+	} else {
+		header("Location: pageUnauthorized.php");
+	}
+
+
+
+
+
+?>
